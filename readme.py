9bd80ad0d139687c03cfcb3b10b0322a2bd71df2
NanMuZ <214707006@csu.edu.cn>
2026-01-31 07:49:46 +0800

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,7 +1,7 @@
 # Less is More: Clustered Cross-Covariance Control for Offline RL
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-[![Conference](https://img.shields.io/badge/ICLR-2026-blue.svg)](https://openreview.net/forum?id=你的论文ID)
+[![Conference](https://img.shields.io/badge/ICLR-2026-blue.svg)](https://openreview.net/forum?id=drOy5wi6Qq)
 [![Python-3.8+](https://img.shields.io/badge/Python-3.8+-green.svg)](https://www.python.org/)
 [![PyTorch](https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?logo=PyTorch&logoColor=white)](https://pytorch.org/)
 
